45e78ca8b8eeb4ad9ca38aa6c3278d508b30d429
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-31 02:49:13 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.add_project(self) unless 
+    backer.add_project(self) unless backer.backed_projects.include?(self)
   end
 end
